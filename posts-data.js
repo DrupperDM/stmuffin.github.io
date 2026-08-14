f7a86d53f6b90db96f05875fbf545a83dd35a362
StaryMuffin <93835538+STMuffin@users.noreply.github.com>
2026-08-13 18:20:12 -0700

    Remove several posts from posts-data.js
    Removed multiple posts from the POSTS array in posts-data.js.

--- posts-data.js
+++ posts-data.js
@@ -30,32 +30,6 @@
 
 const POSTS = [
   {
-    slug: "test2",
-    title: "test2",
-    date: "2026-08-13",
-    tags: ["test"],
-    cover: "images/1786669368836-image-1.png",
-    excerpt: "asdasdasd",
-    content: "asdasdasd\n\nhttps://www.youtube.com/watch?v=_XmZBavbsCw\n\n\n\nhttps://discord.gg/zRhHXWYPw\n\n\n\nhttps://x.com/chiZyt0XD/status/2088012066628002303?s=20"
-  },
-  {
-    slug: "test",
-    title: "TEST",
-    date: "2026-08-13",
-    tags: ["test"],
-    cover: "images/1786668417990-hnn-xgzxyaa8dbm.jpg",
-    excerpt: "UN TEST DE PAGINA",
-    content: "ASKAJKSFLKASFASLKFLASFJKLASFLKAFS\n\n![TEST](images/1786668439620-hnn-xgzxyaa8dbm.jpg)"
-  },
-  {
-    slug: "hola-mundo",
-    title: "¡Hola, mundo!",
-    date: "2026-08-10",
-    tags: ["primer-post", "universidad"],
-    excerpt: "Primer post del blog. Aquí voy a ir dejando lo que aprendo mientras estudio Ingeniería de Software.",
-    content: "Bueno, esto es lo más parecido a un \"hola mundo\" que voy a escribir fuera del código.\n\nLa idea de este blog es simple: ir dejando registro de lo que voy aprendiendo, los proyectos que hago para la carrera (y los que hago por mi cuenta), y algún que otro bug que me haya hecho sufrir más de la cuenta.\n\nNo prometo publicar seguido. Pero aquí queda el primer post."
-  },
-  {
     slug: "fin-de-cubikasmp-temporada-3-muchas-gracias-por-jugar",
     title: "FIN DE CUBIKASMP TEMPORADA 3 - MUCHAS GRACIAS POR JUGAR",
     date: "2026-07-28",
